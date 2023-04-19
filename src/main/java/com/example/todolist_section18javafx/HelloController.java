2fb89c651aa18c4e169f73ea932572ca7511917a
Lance <lanc3.corcega@gmail.com>
2023-04-19 17:01:42 +1200

    Initial commit

---
+++ src/main/java/com/example/todolist_section18javafx/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.todolist_section18javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.todolist_section18javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.todolist_section18javafx to javafx.fxml;
+    exports com.example.todolist_section18javafx;
+}
