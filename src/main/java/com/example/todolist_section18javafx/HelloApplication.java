e23e5234c6a6a33781fc045f3376b48e2f95656d
Lance <lanc3.corcega@gmail.com>
2023-04-19 17:17:57 +1200

    Section 18.255(JavaFX; Setup Sample Todo List Application)
    Setting up the codebase for JavaFX demonstrations

--- src/main/java/com/example/todolist_section18javafx/HelloApplication.java
+++ src/main/java/com/example/todolist_section18javafx/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.todolist_section18javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,12 +10,17 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainwindow.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
+        primaryStage.setTitle("Todo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/todolist_section18javafx/HelloController.java
+++ src/main/java/com/example/todolist_section18javafx/HelloController.java
@@ -1,14 +1,43 @@
 package com.example.todolist_section18javafx;
 
+import com.datamodel.TodoItem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController {
+    private List<TodoItem> todoItems;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card",
+                "Buy a 30th Birthday card for John",
+                LocalDate.of(2023, Month.APRIL, 25));
+        TodoItem item2 = new TodoItem("Doctor's Appointment",
+                "See Dr. Smith at 123 Main Street.",
+                LocalDate.of(2023, Month.MAY, 25));
+        TodoItem item3 = new TodoItem("Finish design proposal for client",
+                "I promised Mike I'd email website mockups by mm yy dd",
+                LocalDate.of(2023, Month.JUNE, 22));
+        TodoItem item4 = new TodoItem("Pickup Doug at the train station",
+                "Doug's arriving on March 23 on the 0500 Train",
+                LocalDate.of(2023, Month.MARCH, 25));
+        TodoItem item5 = new TodoItem("Pick up dry cleaning",
+                "Clothes should be ready by wednesday mm yy dd",
+                LocalDate.of(2023, Month.APRIL, 19));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
     }
+
+
+
+
 }
