d9a596ef8819ea4f021a2075d54807b7dfa04933
Lance <lanc3.corcega@gmail.com>
2023-04-22 19:32:52 +1200

    Section 18.264(JavaFX; Data Binding and Observable)
    using ObservableList() to bind data to a control (ListView)

--- src/main/java/com/datamodel/TodoData.java
+++ src/main/java/com/datamodel/TodoData.java
@@ -1,6 +1,7 @@
 package com.datamodel;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,7 +17,7 @@ import java.util.List;
 public class TodoData {
     private static TodoData instance = new TodoData();
     private static String filename = "TodoListItems.txt";
-    private List<TodoItem> todoItems;
+    private ObservableList<TodoItem> todoItems;
     private DateTimeFormatter formatter;
 
     public static TodoData getInstance() {
@@ -27,7 +28,7 @@ public class TodoData {
         formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     }
 
-    public List<TodoItem> getTodoItems() {
+    public ObservableList<TodoItem> getTodoItems() {
         return todoItems;
     }
 
